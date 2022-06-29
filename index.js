5fa3d49fe23bf95d11f3876690d315c10059444b
JackYouk <jackyoukstetter1@gmail.com>
2022-06-29 15:38:53 -0700

    added bootstrap cdn, added inquirer questions

--- index.js
+++ index.js
@@ -6,45 +6,106 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
-// questions array
-const questions = [
+// Inquirer ---------------------------------------------------------------------------
+// menu object
+const menu = {
+    message: 'Add team members/Finish building team?',
+    name: 'menuSelection',
+    type: 'list',
+    choices: ['Add engineer', 'Add intern', 'Finish'],
+}
+// manager =============================
+// manager questions array
+const managerQuestions = [
     {
-
+        message: 'Team Manager name?',
+        name: 'managerName',
+        type: 'input',
     },
     {
-
+        message: 'Team Manager employee ID?',
+        name: 'managerID',
+        type: 'input',
     },
-]
-
-// inquirer
+    {
+        message: 'Team Manager email?',
+        name: 'managerEmail',
+        type: 'input',
+    },
+    {
+        message: 'Team Manager office number?',
+        name: 'managerOffice',
+        type: 'input',
+    },
+    {
+        message: 'Team Manager office number?',
+        name: 'managerOffice',
+        type: 'input',
+    },
+    menu,
+];
+// manager inquiery
 inquirer
-    .prompt(questions)
+    .prompt(managerQuestions)
     .then((answers) => {
         console.log(answers);
-        fs.writeFile(`title.html`, `
-        // html code
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        `, err => {
-            if (err) {
-              console.log(err);
-              return;
-            }
-            console.log('success');
-          })
     })
+
+// engineer ====================
+// eng questions array
+const engQuestions = [
+    {
+        message: 'Engineer name?',
+        name: 'engName',
+        type: 'input',
+    },
+    {
+        message: 'Engineer ID?',
+        name: 'engID',
+        type: 'input',
+    },
+    {
+        message: 'Engineer email?',
+        name: 'engEmail',
+        type: 'input',
+    },
+    {
+        message: 'Engineer github?',
+        name: 'engGithub',
+        type: 'input',
+    },
+    menu,
+];
+// eng inquiry
+
+// intern =======================
+// intern questions array
+const internQuestions = [
+    {
+        message: 'Intern name?',
+        name: 'internName',
+        type: 'input',
+    },
+    {
+        message: 'Intern ID?',
+        name: 'internID',
+        type: 'input',
+    },
+    {
+        message: 'Intern email?',
+        name: 'internEmail',
+        type: 'input',
+    },
+    {
+        message: 'Intern school?',
+        name: 'internSchool',
+        type: 'input',
+    },
+    menu,
+];
+// intern inquiry
+    
+
+
+// fs write file -------------------------------------------------------------------------------------
