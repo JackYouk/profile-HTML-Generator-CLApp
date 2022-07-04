dd4c0944d36d3e5a4374145e7553e9e027901327
JackYouk <jackyoukstetter1@gmail.com>
2022-07-05 00:09:39 +0100

    finished inquiry, created html generator function, added a html writefile

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 // packages ------------------------------------------------------------------------------------------
 const inquirer = require('inquirer');
 const fs = require('fs');
+// classes -------------------------------------------------------------------------------------------
 const Employee = require('./employee');
 const Manager = require('./manager');
 const Engineer = require('./engineer');
 const Intern = require('./intern');
 
 // HTML code (string) --------------------------------------------------------------------------------
-let htmlCode = '';
+let htmlContent = '';
+let genHtmlCode = (content) => {
+    let htmlCode = '';
+    htmlCode = htmlCode.concat(
+        `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Employee Profiles</title>
+            <!-- Bootstrap CSS cdn -->
+            <link 
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" 
+                rel="stylesheet" 
+                integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" 
+                crossorigin="anonymous"
+            >
+        </head>
+        <body>`
+    )
+    htmlCode = htmlCode.concat(content);
+    htmlCode = htmlCode.concat(
+        `<!-- Bootstrap JS cdn -->
+        <script 
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" 
+            crossorigin="anonymous">
+        </script>
+        </body>
+        </html>`
+    )
+
+    return htmlCode;
+}
 
 // inquirer ------------------------------------------------------------------------------------------
 function inquiry(){
@@ -112,7 +147,7 @@ function inquiry(){
             // declare manager using manager class
             let manager = new Manager(managerName, managerID, managerEmail, managerOffice);
             // add manager card html code to html code string
-            htmlCode += manager.writeCard();
+            htmlContent += manager.writeCard();
 
             // menu cb
             let menuCB = (employeeAnswers) => {
@@ -130,7 +165,7 @@ function inquiry(){
                             // declare eng using eng class
                             let engineer = new Engineer(engName, engID, engEmail, engGithub);
                             // add engineer card html code to html code string
-                            htmlCode += engineer.writeCard();
+                            htmlContent += engineer.writeCard();
                             // init menu for engineer inquiry
                             menuCB(engAnswers);
                         })
@@ -148,9 +183,9 @@ function inquiry(){
                         let internSchool = internAnswers.internSchool;
                         // declare intern using intern class
                         let intern = new Intern(internName, internID, internEmail, internSchool);
-                        // add internineer card html code to html code string
-                        htmlCode += intern.writeCard();
-                        // init menu for internineer inquiry
+                        // add intern card html code to html code string
+                        htmlContent += intern.writeCard();
+                        // init menu for intern inquiry
                         menuCB(internAnswers);
                     })
 
@@ -165,5 +200,15 @@ function inquiry(){
         })
 }
 
+// init inquiry
+inquiry();
 
 // fs write file -------------------------------------------------------------------------------------
+let htmlCode = genHtmlCode(htmlContent);
+fs.writeFile(`employeeProfiles.html`, htmlCode, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('success');
+  })
