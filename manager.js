7180179fddad291aca3ad4b42bb386de0497b8de
JackYouk <jackyoukstetter1@gmail.com>
2022-07-08 11:56:35 +0100

    touched up html code

--- manager.js
+++ manager.js
@@ -13,7 +13,7 @@ class Manager extends Employee{
                 <div class="card-body">
                     <h5 class="card-title">${this.name}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Team Manager</h6>
-                    <p class="card-text">ID: ${this.id}\n Office Number: ${this.officeNum}</p>
+                    <p class="card-text">ID: ${this.id} || Office #: ${this.officeNum}</p>
                     <a href="mailto:${this.email}" class="card-link">Email</a>
                 </div>
             </div>
